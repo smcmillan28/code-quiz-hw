0116dbe74e45da94ce60ac0ea90a9f7ebd3f5dd6
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-01-10 15:56:38 -0500

    started adding questions to JS

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,62 @@
+// Setting variables needed for quiz using DOM
 var start = document.getElementById("start-quiz");
 var timerEl = document.getElementById("timer");
+var quizEl = document.getElementById("quiz-content");
 
-function countdown() {
+// Creating questions through a set of objects/variables
+var myQuestions = [
+    {
+        question: "Arrays in Javascript can be used to store _____.", 
+        answers: {
+            a: "numbers and strings",
+            b: "other arrays",
+            c: "booleans",
+            d: "all of the above"
+        },
+        correctAnswer = "d"
+    }, 
+    {
+        question: "Commonly used data types DO NOT include _____.",
+        answers: {
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers"
+        }, 
+        correctAnswer = "c"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answers: {
+            a: "quotation marks",
+            b: "curly brackets",
+            c: "parentheses",
+            d: "commas"
+        },
+        correctAnswer = "a"
+    }
+]
+
+
+// Write function that will manipulate DOM and create quiz
+function quizStart() {
     var timeLeft = 100;
-
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-        //
-        // YOUR CODE HERE
-        //
+    // Primary timer function
+    var timeInterval = setInterval(function () { 
         timeLeft--;
         timerEl.textContent = timeLeft
 
+        // Write quiz questions to the DOM
+
         if (timeLeft === 0) {
             clearInterval(timeInterval);
-            timerEl.textContent = "Time's Up!"
+            timerEl.textContent = "0"
+            var endGame = document.getElementById("quiz-content");
+            endGame.textContent = "Time's Up!! Try again.";
+            endGame.setAttribute("style", "font-size: 30px; text-align: center;");
         }
 
     }, 1000);
 }
 
-start.addEventListener("click", countdown);
+start.addEventListener("click", quizStart);
