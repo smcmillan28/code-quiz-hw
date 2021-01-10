5bc92255cbacc49c49acb075c5a06dfe36f51b6f
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-01-10 17:10:12 -0500

    added highscore page to assets folder, modified question object structure in script.js

--- assets/script.js
+++ assets/script.js
@@ -3,46 +3,15 @@ var start = document.getElementById("start-quiz");
 var timerEl = document.getElementById("timer");
 var quizEl = document.getElementById("quiz-content");
 
-// Creating questions through a set of objects/variables
-var myQuestions = [
-    {
-        question: "Arrays in Javascript can be used to store _____.", 
-        answers: {
-            a: "numbers and strings",
-            b: "other arrays",
-            c: "booleans",
-            d: "all of the above"
-        },
-        correctAnswer = "d"
-    }, 
-    {
-        question: "Commonly used data types DO NOT include _____.",
-        answers: {
-            a: "strings",
-            b: "booleans",
-            c: "alerts",
-            d: "numbers"
-        }, 
-        correctAnswer = "c"
-    },
-    {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
-        answers: {
-            a: "quotation marks",
-            b: "curly brackets",
-            c: "parentheses",
-            d: "commas"
-        },
-        correctAnswer = "a"
-    }
-]
-
-
-// Write function that will manipulate DOM and create quiz
+// Write function that will manipulate DOM and push quiz to the page
 function quizStart() {
     var timeLeft = 100;
+
+    // Clear button from the page
+    quizEl.textContent = "";
+
     // Primary timer function
-    var timeInterval = setInterval(function () { 
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = timeLeft
 
@@ -59,4 +28,14 @@ function quizStart() {
     }, 1000);
 }
 
+// Initiating quiz and timer with click
 start.addEventListener("click", quizStart);
+
+// Creating questions through series of variables/objects
+var myQuestions = [
+    new Question("Arrays in Javascript can be used to store _____.", ["numbers and strings", "other arrays", "booleans", "all of the above"], "all of the above"),
+    new Question("Commonly used data types DO NOT include _____.", ["strings", "booleans", "alerts", "numbers"], "alerts"),
+    new Question("String values must be enclosed within _____ when being assigned to variables.", ["quotation marks", "curly brackets", "parentheses", "commas"], "quotation marks"),
+    new Question("The condition in an if/else statement is enclosed within _____.", ["curly brackets", "parentheses", "commas", "quotation marks"], "parentheses"),
+    new Question("A very useful tool used during development and debugging for printing content to the debugger is _____.", ["Javascript", "the terminal/bash", "console log", "for loops"], "console log")
+];
